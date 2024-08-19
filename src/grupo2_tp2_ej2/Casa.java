ae55a2a717c032edf2bc0a9e5553a29606f4d82a
Maycoldm <maycoldm09@gmail.com>
2024-08-19 17:38:42 -0300

    clase casa
    hice una casa

---
+++ src/grupo2_tp2_ej2/Casa.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo2_tp2_ej2;
+
+
+public class Casa {
+    private int habitaciones;
+    private String domicilio;
+
+    public Casa(int habitaciones, String domicilio) {
+        this.habitaciones = habitaciones;
+        this.domicilio = domicilio;
+    }
+
+    public int getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(int habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+     
+    
+}
