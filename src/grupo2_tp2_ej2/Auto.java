b5774373eea41bbfd90a99888b3b476cdc389944
Marcosd3laULP <usuario4ltnum1@gmail.com>
2024-08-19 17:33:49 -0300

    Hice la clase auto
    Hice la clase auto con estos atributos
    modelo
    patente
    año modelo

--- src/grupo2_tp2_ej2/Auto.java
+++ src/grupo2_tp2_ej2/Auto.java
@@ -8,5 +8,31 @@ public class Auto {
     public String color;
     public int añoM;
     
+  
     //Sosa Chirino Marcos Antonio
+
+    public Auto(String modelo, String patente, String color, int añoM) {
+        this.modelo = modelo;
+        this.patente = patente;
+        this.color = color;
+        this.añoM = añoM;
+        
+        
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAñoM() {
+        return añoM;
+    }
 }
