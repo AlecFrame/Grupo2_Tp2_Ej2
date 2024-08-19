598f06ccd85fe22834df174d03e64c6934c1d301
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-08-19 17:24:14 -0300

    Actualize la clase Reloj
    La clase Reloj no usaba los LocalDate

--- src/grupo2_tp2_ej2/Reloj.java
+++ src/grupo2_tp2_ej2/Reloj.java
@@ -1,61 +1,54 @@
 
 package grupo2_tp2_ej2;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Reloj {
-    
     private String dia;
-    private int conteoDia;
     private String hora;
     private String modelo;
     private long numSerie;
-    
-    public Reloj(){
-        dia = "lunes";
-        conteoDia = 1;
+
+    public Reloj(String dia, String hora, String modelo, long numSerie) {
+        this.dia = dia;
+        this.hora = hora;
+        this.modelo = modelo;
+        this.numSerie = numSerie;
     }
-    
-    public String getDia(){
+
+    public String getDia() {
         return dia;
     }
-    
-    public String getHora(){
+
+    public String getHora() {
         return hora;
     }
     
-    public void incrementarDia(){
-        conteoDia += 1;
-        if(conteoDia == 8){
-            conteoDia = 1;
-        }
-        switch(conteoDia){
-            case 1 ->{
-                dia = "lunes";
-            }
-            case 2 ->{
-                dia = "martes";
-            }
-            case 3 ->{
-                dia = "miercoles";
-            }
-            case 4 ->{
-                dia = "jueves";
-            }
-            case 5 ->{
-                dia = "viernes";
-            }
-            case 6 ->{
-                dia = "sabado";
-            }
-            case 7 ->{
-                dia = "domingo";
-            }
-        }
-    }    
-    public void incrementarHora(){
-        
+    public void incrementarDia() {
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dia = LocalDate.parse(this.dia, formato);
+        dia = dia.plusDays(1);
+        this.dia = dia.format(formato);
+        System.out.println(" (Se incremento un dia al reloj)");
     }
-    public void limpiarPantalla(){
-        
+    
+    public void incrementarHora() {
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime hora = LocalTime.parse(this.hora,formato);
+        hora = hora.plusHours(1);
+        this.hora = hora.format(formato);
+        System.out.println(" (Se incremento una hora al reloj)");
+    }
+    
+    public void limpiarPantalla() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+    
+    @Override
+    public String toString() {
+        return "modelo: "+modelo+",dia: "+dia+", hora: "+hora+", Numero de serie: "+numSerie;
     }
 }
