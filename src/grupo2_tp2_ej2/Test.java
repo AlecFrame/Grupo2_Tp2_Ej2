fb25b1c52a3eaf710fc7a00af4a975a88cfdaa0b
AlecFrame <walteravertacnik@sanluis.edu.ar>
2024-08-19 16:35:42 -0300

    Initial commit

---
+++ src/grupo2_tp2_ej2/Test.java
@@ -0,0 +1,20 @@
+
+package grupo2_tp2_ej2;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("///// TP 2 - EJ 2 ,Grupo 2, Comision 2, por: /////");
+        System.out.println("///// > Walter Alexander Vertacnik");
+        System.out.println("///// > Ferro Julieta");
+        System.out.println("///// > Maximo Quiroga");
+        System.out.println("///// > Maycol Muñoz");
+        System.out.println("///// > Villegas Loudres");
+        System.out.println("///// > Patricia Pagnone");
+        System.out.println("///// > Sosa Chirino Marcos Antonio");
+        System.out.println("///// > Soto vela Luciano Ezequiel");
+        
+        
+    }
+    
+}
